43c48d692e788995e3b15e8106cbffefef647031
JhoanSTalero13 <jhoan.talero@unigermana.edu.co>
2022-09-28 17:46:53 -0500

    first commit

---
+++ src/main/java/com/ejemplo/despliegue/jba/jbaprueba/JbapruebaApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.despliegue.jba.jbaprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbapruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbapruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/despliegue/jba/jbaprueba/JbapruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.despliegue.jba.jbaprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbapruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
